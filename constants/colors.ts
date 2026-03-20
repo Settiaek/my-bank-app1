a3f4ed6448805396239c2337e73cba94727978fd
Settiaek <148587998+Settiaek@users.noreply.github.com>
2026-03-20 07:26:17 +0100

    Add files via upload

---
+++ constants/colors.ts
@@ -0,0 +1,29 @@
+const GOLD = "#FFD700";
+const GOLD_LIGHT = "#FFE55C";
+const GOLD_DARK = "#C8A600";
+const BLACK = "#0B0E11";
+const DARK_CARD = "#141720";
+const DARK_SURFACE = "#1C2030";
+const DARK_BORDER = "#252A3A";
+const WHITE = "#FFFFFF";
+const GRAY = "#8B95A8";
+const GRAY_LIGHT = "#B8C0CC";
+const RED = "#FF3B30";
+const GREEN = "#34C759";
+const BLUE = "#1A73E8";
+
+export default {
+  black: BLACK,
+  gold: GOLD,
+  goldLight: GOLD_LIGHT,
+  goldDark: GOLD_DARK,
+  darkCard: DARK_CARD,
+  darkSurface: DARK_SURFACE,
+  darkBorder: DARK_BORDER,
+  white: WHITE,
+  gray: GRAY,
+  grayLight: GRAY_LIGHT,
+  red: RED,
+  green: GREEN,
+  blue: BLUE,
+};
