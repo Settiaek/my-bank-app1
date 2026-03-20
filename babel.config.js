392c0540ba5daea4fc601b068ee51637a88690f9
Settiaek <148587998+Settiaek@users.noreply.github.com>
2026-03-20 13:42:29 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,8 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['expo-router/babel'], // هذا السطر ضروري جداً لتشغيل التطبيق
+    plugins: [
+      'react-native-reanimated/plugin',
+    ],
   };
 };
